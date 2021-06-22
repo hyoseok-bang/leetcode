0e9c7f10cc4122185c37dfae3a9f0d3c8b069b94
Hyoseok <hyoseok1989@gmail.com>
2021-06-22 20:21:13 +0900

    Added solution using Kadane's algorithm

--- no53.py
+++ no53.py
@@ -2,3 +2,14 @@ def max_subarray(nums):
     for i in range(1, len(nums)):
         nums[i] += nums[i-1] if nums[i-1] > 0 else 0
     return max(nums)
+
+
+# Kadane's Algorithm
+def maxSubArray(nums):
+    best_sum = -sys.maxsize
+    current_sum = 0
+    for num in nums:
+        current_sum = max(sum, current_sum + sum)
+        best_sum = max(best_sum, current_sum)
+    
+    return best_sum
